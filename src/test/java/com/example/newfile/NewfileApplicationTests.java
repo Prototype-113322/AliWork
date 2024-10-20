1ffb79a6707641a23efb65f2cb4f794192547658
Emma Frost <sarwarkhalid69@gmail.com>
2024-10-20 15:30:43 +0500

    Add files via upload

---
+++ src/test/java/com/example/newfile/NewfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
